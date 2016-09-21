39794f9844f66b17b1de320d79a037dd033009da
Randall Degges <r@rdegges.com>
2016-09-21 12:52:31 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
 
     # Basic package information:
     name = 'stormpath-seed',
-    version = '0.0.2',
+    version = '0.0.3',
     scripts = ['stormpath-seed'],
 
     # Packaging options:
